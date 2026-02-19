c85385a1ad0b53be86c9db652564cb191615159c
codorco <william.emerson@hotmail.com>
2026-02-19 20:14:51 -0300

    estrutura de conexão MySQL e integração com o Model Agents

---
+++ app/config_sample.php
@@ -0,0 +1,11 @@
+<?php
+
+define('APP_NAME',  'Basic Name Gathering');
+
+// database
+define('MYSQL_HOST',        '');
+define('MYSQL_DATABASE',    '');
+define('MYSQL_USERNAME',    '');
+define('MYSQL_PASSWORD',    '');
+
+// coloque os dados do seu banco e depois renomeie para config.php
--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -2,11 +2,17 @@
 
 namespace scc\Controllers;
 use scc\Controllers\BaseController;
+use scc\Models\Agents;
 
 class Main extends BaseController
 {
     public function index()
     {
+
+        $model = new Agents();
+        $results = $model->get_total_agents();
+        printData($results);
+
         $data['nome'] = "Joao";
         $data['apelido'] = "Riveiro";
         $this->view('layouts/html_header');
